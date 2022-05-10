c4ea5842217d56487509fd6767b17901244dfa29
Brian Simpo <90185393+luanardev@users.noreply.github.com>
2022-05-10 08:25:44 +0200

    Update index.blade.php

--- Resources/views/livewire/registration/dependant/index.blade.php
+++ Resources/views/livewire/registration/dependant/index.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if ($browseMode)
-        @include('employees::livewire.registration.dependant.index')
+        @include('employees::livewire.registration.dependant.view')
     @endif
     @if($createMode)
         @include('employees::livewire.registration.dependant.create')
